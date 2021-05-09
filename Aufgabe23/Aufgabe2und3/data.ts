ee68ad96fce16ac96ef5469df8f9a530e508c8b8
YuukiItasha <81272072+YuukiItasha@users.noreply.github.com>
2021-05-09 18:14:56 +0200

    Update data.ts

--- Aufgabe23/Aufgabe2und3/data.ts
+++ Aufgabe23/Aufgabe2und3/data.ts
@@ -4,8 +4,62 @@ namespace Aufgabe2und3 {
     export let posibilityUnten: Posibility[] = [];
     
     let path: string = window.location.pathname.substring(0, window.location.pathname.lastIndexOf("/") + 1);
+  /*
+    interface ImagePart {
+        imageUrl: string;
+        position: string;
+}    
 
+    interface FullImages {
+            imageId: string;
+            oben: ImagePart;
+            mitte: ImagePart;
+            unten: ImagePart;
+}
 
+    const data: FullImages[] = [
+    {
+        imageId: "asuna1",
+        oben: {
+            imageUrl: "Bilder/Oben/asuna1_oben.jpg"
+        }
+        mitte: {
+            imageUrl: "Bilder/Mitte/asuna1_mitte.jpg"
+        }
+        unten: {
+            imageUrl: "Bilder/Unten/asuna1_unten.jpg"
+        }
+
+        imageId: "asuna2",
+        oben: {
+            imageUrl: "Bilder/Oben/asuna2_oben.jpg"
+        }
+        mitte: {
+            imageUrl: "Bilder/Mitte/asuna2_mitte.jpg"
+        }
+        unten: {
+            imageUrl: "Bilder/Unten/asuna2_unten.jpg"
+        }
+
+        imageId: "asuna3",
+        oben: {
+            imageUrl: "Bilder/Oben/asuna3_oben.jpg"
+        }
+        mitte: {
+            imageUrl: "Bilder/Mitte/asuna3_mitte.jpg"
+        }
+        unten: {
+            imageUrl: "Bilder/Unten/asuna3_unten.jpg"
+        }
+    }
+];
+
+function displayPart(part: ImagePart, position: string): void {
+    const imageContainer: HTMLDivElement = <HTMLDivElement>document.getElementById("image-container");
+
+    imageContainer.appendChild
+} */
+ 
     
     let bild1Oben: Posibility = new Posibility("asuna1 - Oben", 0, path + "Bilder/Oben/asuna1_oben.jpg");
     let bild1Mitte: Posibility = new Posibility("asuna1 - Mitte", 1, path + "Bilder/Mitte/asuna1_mitte.jpg");
